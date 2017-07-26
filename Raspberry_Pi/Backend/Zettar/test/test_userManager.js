8a358cb7be512611ece79250491e7a128d75476b
Marthinus-tmp <marthinus.up.tmp@gmail.com>
2017-07-26 22:53:08 +0200

    Created some CRUD tests in Mocha that is run from a driver script
    To run the test one must use: 'npm test'

---
+++ Raspberry_Pi/Backend/Zettar/test/test_userManager.js
@@ -0,0 +1,38 @@
+﻿var chai = require("chai");
+var expect = chai.expect;
+var assert = chai.assert;
+
+var dbMan = require('../databaseManager');
+var UserManager = require("../userManager");
+
+
+
+
+
+describe("UserManager", function () {
+    describe("database CRUD", function () {
+        describe("#addUser", function () {
+            it("adds a user to the db", function () {
+                return UserManager
+                    .addUser({ name: 'name-test', surname: 'surname-test', age: 100 })
+                    .then((user) => {
+                        expect(user.name).to.equal('name-test');
+                        expect(user.surname).to.equal('surname-test');
+                        expect(user.age).to.equal(100);
+                    })
+            })
+        })
+
+        describe("#getUser", function () {
+            it("gets a user from the db", function () {
+                return UserManager
+                    .getUser('name-test')
+                    .then((user) => {
+                        expect(user.name).to.equal('name-test');
+                        expect(user.surname).to.equal('surname-test');
+                        expect(user.age).to.equal(100);
+                    })
+            })
+        })
+    })
+})
