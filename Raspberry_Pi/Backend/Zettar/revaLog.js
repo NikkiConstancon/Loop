fd3b69c7d93ae2f0b7b86d1f5d0d81bed30dd16f
Marthinus-tmp <marthinus.up.tmp@gmail.com>
2017-07-26 02:40:08 +0200

    Added Winston

---
+++ Raspberry_Pi/Backend/Zettar/revaLog.js
@@ -0,0 +1,36 @@
+﻿//levels: { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
+var winston = require('winston');
+require('winston-daily-rotate-file');
+var cassandra = require('winston-cassandra').Cassandra;
+
+
+module.exports = winston;
+
+
+winston.level = 'debug';
+
+
+//make log directory once
+//https://stackoverflow.com/a/26815894
+var fs = require('fs');
+var dir = './logs';
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+//store logs in db
+// winston.add(cassandra, { contactPoints: ['127.0.0.1'], keyspace: 'logs' });
+
+//store logs in file
+winston.add(winston.transports.DailyRotateFile, {
+    filename: './logs/log',
+    datePattern: 'yyyy-MM-dd.',
+    prepend: true,
+    level: process.env.ENV === 'development' ? 'debug' : 'info'
+});
+
+//pretty console colours
+winston.cli();
+
+
+
