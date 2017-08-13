97f7d6207ecaba6ceb6f2bfb1b1883f2df881e4b
Marthinus-tmp <marthinus.up.tmp@gmail.com>
2017-08-14 00:14:49 +0200

    Added rudimentary session managed server

---
+++ Raspberry_Pi/Backend/Zettar/lib/revaServer.js
@@ -0,0 +1,98 @@
+﻿var express = require('express')
+var parseurl = require('parseurl')
+var initUserSession = require('./userSession')
+
+
+var app = express()
+initUserSession(app)
+server = module.exports = require('http').createServer(app);
+
+
+server.listen(3000, function (err) {
+    err && console.log(err)
+    console.log('\tserver started ', server.address())
+})
+
+
+
+/*
+require('uuid/v1');
+
+var net = require('net');
+
+
+var HOST = '127.0.0.1'; // parameterize the IP of the Listen
+var PORT = 6969; // TCP LISTEN port
+
+
+// Create an instance of the Server and waits for a conexão
+net.createServer(function(sock) {
+
+
+  // Receives a connection - a socket object is associated to the connection automatically
+  console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+
+  // Add a 'data' - "event handler" in this socket instance
+  sock.on('data', function(data) {
+	  // data was received in the socket 
+	  // Writes the received message back to the socket (echo)
+      sock.write(data);
+      console.log(data)
+  });
+
+
+  // Add a 'close' - "event handler" in this socket instance
+  sock.on('close', function(data) {
+	  // closed connection
+	  console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+  });
+
+  sock.on('error', function(data) {
+	  // closed connection
+      console.log(data)
+      console.log('error: ' + sock.remoteAddress +' '+ sock.remotePort);
+  });
+
+}).listen(PORT, HOST);
+
+
+console.log('Server listening on ' + HOST +':'+ PORT);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+var tls = require('tls'),
+    fs = require('fs'),
+    colors = require('colors'),
+    msg = [
+        ".-..-..-.  .-.   .-. .--. .---. .-.   .---. .-.",
+        ": :; :: :  : :.-.: :: ,. :: .; :: :   : .  :: :",
+        ":    :: :  : :: :: :: :: ::   .': :   : :: :: :",
+        ": :: :: :  : `' `' ;: :; :: :.`.: :__ : :; ::_;",
+        ":_;:_;:_;   `.,`.,' `.__.':_;:_;:___.':___.':_;"
+    ].join("\n").cyan;
+
+var options = {
+    key: fs.readFileSync('private-key.pem'),
+    cert: fs.readFileSync('public-cert.pem')
+};
+
+tls.createServer(options, function (s) {
+    s.write(msg + "\n");
+    s.pipe(s);
+}).listen(8000);
+*/
---
+++ Raspberry_Pi/Backend/Zettar/lib/userSession.js
@@ -0,0 +1,115 @@
+﻿
+var dbMan = require('../databaseManager');
+var PatientManager = require('../patientManager');
+
+
+
+var CryptoJS = require("crypto-js");
+var patientKey = "xP{}Lk.x#3V2S?F2p'q{kqd[Qu{7/S-d*bzt"
+var accessKey = "4]),`~>{CKjv(E@'d:udH6N@/G4n(}4dn]Mi"
+
+var uuidv1 = require('uuid/v1')
+
+const HOSTNAME = require('os').hostname()
+
+
+module.exports = function (app) {
+    var session = require('express-session')
+    var bodyParser = require('body-parser')
+    var uuidv1 = require('uuid/v1')
+
+    app.use(session({
+        secret: uuidv1(),
+        resave: false,
+        saveUninitialized: true
+    }))
+    app.use(bodyParser.json());       // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+        extended: true
+    }));
+
+    app.use(bootstrapSession)
+
+    app.get('/', function (req, res, next) {
+        res.status(200).send('hi')
+        next()
+    })
+    app.post('/login', function (req, res, next) {
+        req.revaUser.login(req, res, next)
+    })
+    app.post('/registration', function (req, res, next) {
+        req.revaUser.signup(req, res, next)
+    })
+}
+
+
+
+function bootstrapSession(req, res, next) {
+    if (!req.session.userContext) {
+        req.session.userContext = UserSession.createContext()
+    }
+    req.revaUser = new UserSession(req)
+
+    req.revaUser.isLoggedOn(req, res, next)
+    req.revaUser.validateAuthorizatiron(req, res, next)
+    next()
+}
+
+function UserSession(req) {
+    this.context = req.session.userContext
+    this.context.requestCount++;
+}
+UserSession.createContext = function () {
+    return { loggedOn: false, requestCount: 0 };
+}
+UserSession.prototype.isLoggedOn = function (req, res, next) {
+    if (!this.context.loggedOn && req.path !== '/login' && req.path !== '/registration') {
+        this.context.lastUrl = req.path
+        res.status(401).send('login required')
+        //res.redirect("/login");
+    }
+}
+UserSession.prototype.validateAuthorizatiron = function (req, res, next) {
+}
+
+UserSession.prototype.signup = function (req, res, next) {
+    req.body.PatientPassword = CryptoJS.AES.encrypt(req.body.PatientPassword, patientKey).toString()
+    req.body.AccessPassword = CryptoJS.AES.encrypt(uuidv1(), accessKey).toString()
+
+    PatientManager.addPatient(req.body).then(function () {
+        res.status(201).send('user created')
+    }).catch(function (e) {
+        res.status(412).send(e)
+    })
+}
+UserSession.prototype.login = function (req, res, next) {
+    var self = this;
+    PatientManager.getPatient({ Username: req.body.Username }).then(function (pat) {
+        var truePass = CryptoJS.AES.decrypt(pat.PatientPassword.toString(), patientKey).toString(CryptoJS.enc.Utf8)
+
+
+
+        if (truePass !== req.body.PatientPassword) {
+            res.status(401).send('incorrect user or pass');
+//NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB
+            res.end()
+//NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB--NB
+        } else {
+            self.context.loggedOn = true;
+            res.status(200).send('login successful');
+            next()
+        }
+    }).catch(function (e) {
+        res.status(401).send(e);
+    })
+}
+
+
+
+
+
+
+
+
+
+
