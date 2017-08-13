3842b55949b0cabbedbe67a222fc3ea4455bc6b5
NikkiConstancon <u15011713@tuks.co.za>
2017-08-13 18:21:28 +0200

    modify database models

--- Raspberry_Pi/Backend/Zettar/models/patientModel.js
+++ Raspberry_Pi/Backend/Zettar/models/patientModel.js
@@ -1,16 +1,67 @@
 module.exports = {
     fields:{
-    	Username	: "text",
-    							
-        Password    : "text",	
-        SubscriberPassword : "text", 
-        SubscriberList     : "int", 
-        Email     : "text",	
-        Address     : "text", 
-        Age     : "int",	
-        Weight     : "int",
-        Height     : "int",
-        Reason     : "text"	
+    	Username: "text",			
+        PatientPassword: {
+    		type: "text",
+    		rule: {
+    			required: true
+    		}
+    	},
+    	AccessPassword: {
+    		type: "text",
+    		rule: {
+    			required: true
+    		}
+    	},
+        SubscriberList: {
+            type: "list",
+            typeDef: "<text>"
+        }, 
+        PatientEmail: {
+        	type: "text",
+        	rule: {
+        		validator : function(email){ 
+				    var re = /\S+@\S+\.\S+/;
+    				return re.test(email); 
+                },
+                message: 'Email is in the incorrect form',			
+			    required: true 
+            }
+        },	
+        Address : "text", 
+        Age: {
+        	type: "int",
+        	rule: {
+            	validator : function(value){ return value > 0; },
+            	message   : 'Age must be greater than 0'
+        	}
+    	},	
+        Weight: {
+        	type: "float",
+        	rule: {
+            	validator : function(value){ return value > 0; },
+            	message   : 'weight must be greater than 0'
+        	}
+    	},
+        Height: {
+        	type: "float",
+        	rule: {
+            	validator : function(value){ return value > 0; },
+            	message   : 'Hieght must be greater than 0'
+        	}
+    	},
+        Reason: {
+        	type: "text",
+        	rule: {
+            	validator: function(value){ 
+				    var val = value.toLowerCase();
+				    if(val == 'age' || val == 'illness' || val == 'disability' || val == 'accident' || val == 'other')
+					   return true;
+				    return false; 
+                },
+            	message: 'Reason is not included in the set of reasons.'
+        	}
+    	}	
     },
     key:["Username"]		
 }
--- Raspberry_Pi/Backend/Zettar/models/subscriberModel.js
+++ Raspberry_Pi/Backend/Zettar/models/subscriberModel.js
@@ -1,8 +1,8 @@
 module.exports = {
     fields:{
-    	Email : "text",	
-        Password    : "text",	
-	Relation : "text",
+    	Email : "text",
+        Password    : "text",
+        Relation : "text",
         PatientList     : "int" 
     },
     key:["Email"]
