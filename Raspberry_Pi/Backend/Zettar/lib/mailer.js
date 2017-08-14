5f29b4876999b618f7a5ab955c5cf9de6bbc1e1d
Marthinus-tmp <marthinus.up.tmp@gmail.com>
2017-08-14 23:06:26 +0200

    Added a simple postman for email verification

---
+++ Raspberry_Pi/Backend/Zettar/lib/mailer.js
@@ -0,0 +1,45 @@
+﻿
+var uuid1 = require('uuid/v1')
+var nodemailer = require('nodemailer')
+var logger = require('../revaLog')
+
+var server = require('./revaServer')
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'COS332.Marthinus@gmail.com',
+        pass: 'mail-test-pass'
+    }
+})
+
+
+
+var Mailer = module.exports = {
+    mailEmialConfirmationUrl: function (user, hostUrl) {
+        return new Promise(function (resolve, reject) {
+            if (!hostUrl) {
+                reject('hostUrl is undifined')
+            }
+            var key1 = uuid1()
+            var key2 = uuid1()
+            var mailOptions = {
+                from: 'noreply.mailEmialConfirmationUrl@gmail.com',
+                to: user.PatientEmail,
+                subject: 'Sending Email using Node.js',
+                text: hostUrl + '/emial-confirmation?a=' + key1 + '&b=' + key2
+            }
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    logger.error(error);
+                    reject()
+                } else {
+                    logger.debug('Email sent: ' + info.response);
+                    resolve()
+                }
+            })
+        })
+    }
+}
+
