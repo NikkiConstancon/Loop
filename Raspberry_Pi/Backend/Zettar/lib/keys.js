3c72b82727deddac487b68c1002da72d2e0fd982
Marthinus-tmp <marthinus.up.tmp@gmail.com>
2017-08-16 12:03:28 +0200

    Added lib/keys.js for centralized encryption management

---
+++ Raspberry_Pi/Backend/Zettar/lib/keys.js
@@ -0,0 +1,15 @@
+﻿
+
+
+
+
+var keys = module.exports  = require("crypto-js");
+
+module.exports.userEmail = "xP{}Lk.x#3V2S?F2p'q{kqd[Qu{7/S-d*bzt"
+module.exports.userEmailEncrypt = function (val) {
+    return keys.AES.encrypt(val, keys.userEmail).toString()
+}
+module.exports.userEmailDecrypt = function (k) {
+    return keys.AES.decrypt(k.toString(), keys.userEmail).toString(keys.enc.Utf8)
+}
+
