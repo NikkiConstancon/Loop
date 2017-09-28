cfc487b68113f0279c575a11ff7d350699c50183
Marthinus-tmp <marthinus.up.tmp@gmail.com>
2017-09-28 21:32:10 +0200

    Created a service folder and moved RealTimeService and UserManagerService to it

---
+++ Raspberry_Pi/Backend/Zettar/services/RealtimeDataService.js
@@ -0,0 +1,48 @@
+﻿const webSockMessenger = require('../lib/webSockMessenger')
+const patientManager = require("../patientManager");
+var subscriberManager = require('../subscriberManager');
+
+const logger = require('../revaLog')
+
+const serviceName = 'RTDS'
+
+
+var sevice = module.exports = {
+    publish: function (info, response) {
+        publisher = publisherHandler.getPublisher(info.from)
+        if (!publisher.realTimeCollectorMap) {
+            publisher.realTimeCollectorMap = {}
+            setTimeout(() => {
+                publisher.publish({ [info.from]: publisher.realTimeCollectorMap })
+                publisher.realTimeCollectorMap = null
+            }, 750)
+        }
+        publisher.realTimeCollectorMap[info.name] = response.data.toPrecision(3)
+    },
+    connectZettalet: function (zettaletName) {
+    },
+    disconnectZettalet: function (zettaletName) {
+        publisher = publisherHandler.getPublisher(zettaletName)
+        publisher.setMeta({})
+    },
+    connectDevice: function (zettaletName, info) {
+        publisher = publisherHandler.getPublisher(zettaletName)
+        publisher.setMetaField(info.name, { type: info.type, topicName: info.topicName })
+    }
+}
+
+var publisherHandler = webSockMessenger.attach(serviceName, {
+    connect: function (transmiter) {
+    },
+    close: function (context) {
+    },
+    receiver: function (transmiter, obj) {
+        transmiter.transmit(obj)
+    },
+    subListUpdater: function (pubName, next) {
+        patientManager.bindSubscriberListInofHook({ Username: pubName }, (list) => {
+            next(list)
+        })
+    }
+})
+
---
+++ Raspberry_Pi/Backend/Zettar/services/UserManagerService.js
@@ -0,0 +1,82 @@
+﻿const webSockMessenger = require('../lib/webSockMessenger')
+const patientManager = require("../patientManager");
+var subscriberManager = require('../subscriberManager');
+
+const logger = require('../revaLog')
+
+const serviceName = 'UserManager'
+
+webSockMessenger.attach(serviceName, {
+    connect: function (transmitter) {
+        //transmitter.transmit("--HELLO-- ");
+    },
+    close: function (transmitter) {
+    },
+    receiver: function (transmitter, obj) {
+    },
+    requirePersistentLink: true,
+    defaultEnabled: true
+    , channels: {
+        REGISTER: {
+            VALIDATE_EMAIL: function (transmitter, msg, key, channeler) {
+                channeler({ PASS: true })
+                //channeler({ PASS: false, ERROR: "This email address is not available" })
+            },
+            REGISTER_PATIENT: function (transmitter, msg, key, channeler) {
+                var obj = msg;
+                //TODO: Move to patiantManager
+                function deserialize(obj) {
+                    var test = require('../models/patientModel').fields
+                    for (var key in test) {
+                        switch (test[key].type) {
+                            case 'int': { obj[key] && (obj[key] = parseInt(obj[key])) } break
+                            case 'float': { obj[key] && (obj[key] = parseFloat(obj[key])) } break
+                        }
+                    }
+                }
+                deserialize(obj)
+                PatientManager.getPatient(obj)
+                    .then(function () {
+                        channeler({ PATIENT_ERROR: 'This username has been taken', PATIENT_PASS: false })
+                    }).catch(function () {
+                        return PatientManager.addPatient(obj).then(function (pat) {
+                            channeler({ PATIENT_PASS: true })
+                        }).catch(function (e) {
+                            channeler({ PATIENT_ERROR: e.message || e, PATIENT_PASS: false })
+                        })
+                    }).catch(function (e) {
+                        logger.error('@webSockMessenger$UserManager#receiver:KEY_REGISTER_USER', e)
+                        channeler({ PATIENT_ERROR: 'something went wrong', PATIENT_PASS: false })
+                    })
+            },
+            REGISTER_NON_PATIENT: function (transmitter, msg, key, channeler) {
+                var obj = msg;
+                //TODO: Move to patiantManager
+                function deserialize(obj) {
+                    var test = require('../models/patientModel').fields
+                    for (var key in test) {
+                        switch (test[key].type) {
+                            case 'int': { obj[key] && (obj[key] = parseInt(obj[key])) } break
+                            case 'float': { obj[key] && (obj[key] = parseFloat(obj[key])) } break
+                        }
+                    }
+                }
+                deserialize(obj)
+                SubscriberManager.getsubscriber(obj)
+                    .then(function () {
+                        //ugly prot i.e. copy and paste mostly form patient
+                        channeler({ NON_PATIENT_ERROR: 'This email has been taken', NON_PATIENT_PASS: false })
+                    }).catch(function () {
+                        return SubscriberManager.addSubscriber(obj).then(function (pat) {
+                            channeler({ NON_PATIENT_PASS: true })
+                        }).catch(function (e) {
+                            channeler({ NON_PATIENT_ERROR: e.message || e, NON_PATIENT_PASS: false })
+                        })
+                    }).catch(function (e) {
+                        logger.error('@webSockMessenger$UserManager#receiver:KEY_REGISTER_USER', e)
+                        channeler({ NON_PATIENT_ERROR: 'something went wrong', NON_PATIENT_PASS: false })
+                    })
+            }
+        }
+    }
+})
