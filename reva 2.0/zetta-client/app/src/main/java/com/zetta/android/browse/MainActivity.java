bf89c824f6ccbd78830d0c69f450a7de1bbb6022
Hristian Vitrychenko <h_vitrychenko@yahoo.co.za>
2017-10-03 12:42:57 +0200

    fixed naming and reference issues

--- reva 2.0/zetta-client/app/src/main/java/com/zetta/android/browse/MainActivity.java
+++ reva 2.0/zetta-client/app/src/main/java/com/zetta/android/browse/MainActivity.java
@@ -2,6 +2,7 @@ package com.zetta.android.browse;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
