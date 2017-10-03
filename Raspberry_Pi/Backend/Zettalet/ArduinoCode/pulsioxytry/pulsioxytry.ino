67addcbbcd11c27e81cb4d8bc9d001bdac6d04dd
RavenBlood7 <juan.dupreez82@gmail.com>
2017-10-03 16:08:39 +0200

    Trying again with the pulsioximeter

--- Raspberry_Pi/Backend/Zettalet/ArduinoCode/pulsioxytry/pulsioxytry.ino
+++ Raspberry_Pi/Backend/Zettalet/ArduinoCode/pulsioxytry/pulsioxytry.ino
@@ -6,42 +6,29 @@ int valuePulse;
 int valueSPO2;
 uint8_t pulsioximeter_state = 0;
 
+
 void setup() 
 {
-  Serial.begin(19200);
-  MySignals.begin();
- 
-  MySignals.initSensorUART();
-  MySignals.enableSensorUART(PULSIOXIMETER);
+    pinMode(4, INPUT);
+    pinMode(5, INPUT);
+    pinMode(6, INPUT);
+    delay(20);
+
+    Serial.begin(19200);
+    while (Serial.read() >= 0);
+
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    delay(10);
+
+      delay(30);
 }
 
 void loop() 
 {
-  //while (Serial.available() > 0)
-  Serial.println(Serial.read(), HEX); 
-  // First way of getting sensor data
-  //MySignals.enableSensorUART(PULSIOXIMETER);
-  
-    Serial.print(F("Try this anyway:"));
-    Serial.print(MySignals.pulsioximeterData.BPM);
-  Serial.println();
-  pulsioximeter_state = MySignals.getPulsioximeterMini();
-  if (pulsioximeter_state == 1)
-  {
-    Serial.print(F("Pulse:"));
-    Serial.print(MySignals.pulsioximeterData.BPM);
-    Serial.print(F("bpm / SPO2:"));
-    Serial.print(MySignals.pulsioximeterData.O2);
-    Serial.println(F("%"));
-  }
-  else if (pulsioximeter_state == 2)
-  {
-    Serial.println(F("Not valid data"));
-  }
-  else
-  {
-    Serial.println(F("No available data"));
-  }
-  MySignals.disableSensorUART();
+
+ // while (Serial.available())
+    Serial.println(Serial.read(), HEX);
   delay(2000);
 }
