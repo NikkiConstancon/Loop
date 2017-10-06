877066654ed6184eb6201cba5840e149f66ffb9a
Marthinus-tmp <marthinus.up.tmp@gmail.com>
2017-10-06 04:04:17 +0200

    Implemented major part of notification service, Changed the drawer a bit

--- Raspberry_Pi/Backend/Zettar/services/NotificationsService.js
+++ Raspberry_Pi/Backend/Zettar/services/NotificationsService.js
@@ -6,11 +6,91 @@ const dataManager = require('../patientDataManager');
 const logger = require('../revaLog')
 
 const serviceName = 'Notifications'
+const defaultSmoothFactor = 0.3 // (f * now) + (1-f)*last
+//"°C"
+const THRESHOLD_MAP = {
+    "Body_temperature": new Threshold(0, 1000, defaultSmoothFactor, true),
+    "Body_insulin": new Threshold(0, 1000, defaultSmoothFactor, true),
+    "Heart-rate": new Threshold(0, 1000, defaultSmoothFactor, true),
+    "Body_glucose": new Threshold(37.2, 38.9, defaultSmoothFactor, true),
+}
+
+function Threshold(min, max, smoothFactor, flagRealtimeAnalysis){
+    this.min = min
+    this.max = max
+    this.smoothFactor = smoothFactor
+    this.flagRealtimeAnalysis = flagRealtimeAnalysis
+}
+Threshold.prototype.newAnalyser = function(publisher, info) {
+    return new Analyser(this, publisher, info)
+}
+
+function Analyser(threshold, publisher, info) {
+    this.threshold = threshold
+    this.publisher = publisher
+    this.info = info
+    this.deviceName = info.name
+    this.value = null
+    this.smooth = null
+    this.lastTimeNotificationSent = 0
+    this.thrashGuardDelay = 30 * 1000
+}
+
+Analyser.prototype.analyze = function (now) {
+    if (((new Date()).getTime() - this.lastTimeNotificationSent) > this.thrashGuardDelay) {
+        if (this.threshold.flagRealtimeAnalysis) {
+            if (this.value == null) {
+                this.value = now
+                this.smooth = now
+            }
+            this.updateSmooth(now)
+
+            if (this.deviceName == "Body_glucose") {
+                stop = 1
+            }
+            if (this.smooth < this.threshold.min) {
+                this.publishThresholdDeviation(1, this.deviceName + " is below the set threshold at " + this.valueWithUnit(now))
+            } else if (this.smooth > this.threshold.max) {
+                this.publishThresholdDeviation(1, this.deviceName + " is above the set threshold at " + this.valueWithUnit(now))
+            }
+        } else {
+
+        }
+    }
+}
+Analyser.prototype.valueWithUnit = function(value) {
+    var unit = this.threshold.unit || ""
+    return value.toPrecision(3) + " " + unit
+}
+Analyser.prototype.updateSmooth = function (now) {
+    this.value = now
+    var fac = this.threshold.smoothFactor
+    this.smooth = fac * now + (1 - fac) * this.smooth
+}
+Analyser.prototype.publishThresholdDeviation = function (level, message) {
+    this.publisher.publish({ ThresholdDeviation: { userUid: this.info.from, deviceName: this.deviceName, value: "" + this.value.toPrecision(3), noteLevel: "" + level, message: message } })
+    this.lastTimeNotificationSent = (new Date()).getTime()
+}
+
 
 
 
 var sevice = module.exports = {
-    notify: function (info, response) {
+    analyze: function (info, response) {
+        var threshold = THRESHOLD_MAP[info.name]
+        if (!threshold) {
+            logger.warn("@NotificationsService, no threshold for device: " + info.name)
+            return
+        }
+
+        //setup for patient and leech onto publisher (this will be in ram but who cares at this time?)
+        var publisher = publisherHandler.getPublisher(info.from)
+        publisher[serviceName] || (publisher[serviceName] = {})
+        publisher[serviceName][info.from] || (publisher[serviceName][info.from] = {})
+        publisher[serviceName][info.from][info.name] || (publisher[serviceName][info.from][info.name] = threshold.newAnalyser(publisher, info))
+
+        var analyser = publisher[serviceName][info.from][info.name]
+        analyser.analyze(response.data)
         //console.log(this.result.state)
        /* publisher = publisherHandler.getPublisher(info.from)
         if (!publisher.realTimeCollectorMap) {
@@ -35,6 +115,8 @@ const publisherHandler = webSockMessenger.attach(serviceName, {
         transmiter.transmit(obj)
     },
     subListUpdater: function (pubName, next) {
-        next([pubName])
+        patientManager.bindSubscriberListInfoHook({ Username: pubName }, (list) => {
+            next(list)
+        })
     }
 })
